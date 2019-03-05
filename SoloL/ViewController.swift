79d4b51b7f280596efadad3ed60a9f111e47a247
Egor Pats <egor.pats@gmail.com>
2019-03-05 04:11:47 +0300

    Add hello world iOS application.

---
+++ SoloL/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  SoloL
+//
+//  Created by Egor Pats on 2/26/19.
+//  Copyright © 2019 Egor Pats. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBAction func setName(_ sender: UIButton) {
+        nameLabel.text = nameTextField.text
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
